8cfce15747b1e0200f97744d61ff7f188926fc0a
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-08-19 03:55:05 +0300

    installed a few plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), image(), prefetch(), sitemap()],
+  output: 'server'
 });
