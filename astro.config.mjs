42acf521abcc8c043c71beb9c11a58ad199363d3
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-08-19 21:56:00 +0300

    Replaced astro-image with astro-imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), image(), prefetch(), sitemap()],
+  integrations: [solidJs(), prefetch(), sitemap(), astroImageTools],
   output: 'server'
 });
