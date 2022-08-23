576798cf5c1f8c104adc36563901add2c5489f99
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-08-23 22:36:21 +0300

    uninstall vercel and astro-imagetools. back to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,15 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
 import netlify from '@astrojs/netlify/functions';
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     solidJs(),
-    // prefetch(),
-    // sitemap(),
-    // astroImageTools,
+    prefetch(),
+    sitemap(),
   ],
-  // adapter: netlify(),
-  adapter: vercel(),
+  adapter: netlify(),
   output: 'server'
 });
