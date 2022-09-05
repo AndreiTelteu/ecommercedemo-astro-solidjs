1fc046007d2dd14bcc5a75762707ff020c72d22c
Andrei Telteu <andrei.telteu@touch-media.ro>
2022-09-05 03:47:48 +0300

    Product listing frontend. search and category filters

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import './Loader.scss';
+
+export default function Loader() {
+  return (
+    <div class="spin-wrapper">
+      <div class="spinonediv-1"></div>
+    </div>
+  );
+}
--- src/components/ProductCatalog/ProductCatalog.tsx
+++ src/components/ProductCatalog/ProductCatalog.tsx
@@ -1,6 +1,7 @@
-import { createResource, createSignal, For, onMount } from 'solid-js';
+import { createResource, createSignal, For, onMount, Show } from 'solid-js';
 import api from '~/lib/api';
 import Fuse from 'fuse.js';
+import Loader from '~/components/Loader/Loader';
 import './ProductCatalog.scss';
 
 export default function ProductCatalog(props) {
@@ -40,7 +41,6 @@ export default function ProductCatalog(props) {
       categories: [],
     });
   }
-  
   let searchTimeout = null;
   function handleSearch(event) {
     if (searchTimeout) { clearTimeout(searchTimeout); searchTimeout = null; }
@@ -48,27 +48,75 @@ export default function ProductCatalog(props) {
       setFilters({...filters(), search: event.target.value})
     }, 150);
   }
+  function handleCategoryToggle(item) {
+    const selected = filters().categories.includes(item);
+    console.log({item, on:!selected})
+    const categories = [ ...filters().categories ];
+    if (selected) {
+      categories.splice(categories.indexOf(item), 1);
+    } else {
+      categories.push(item)
+    }
+    setFilters({...filters(), categories});
+  }
   
   return (
-    <div>
+    <div class="product-catalog__columns">
       <div class="product-catalog__filters">
-        <div class="product-catalog__search">
-          <input value={filters().search} onInput={handleSearch} />
-        </div>
-        <div class="product-catalog__categories">
-          
+        <div class="product-catalog__filters-inner">
+          <div class="product-catalog__search">
+            <label>Filters</label>
+            <input
+              value={filters().search} onInput={handleSearch}
+              placeholder="Search by name"
+            />
+          </div>
+          <div class="product-catalog__categories">
+            <label>Categories</label>
+            <div class="product-catalog__categories-list">
+              <For each={data()?.categories} children={(item: any, i) => {
+                const selected = () => filters().categories.includes(item);
+                return (
+                  <label class={selected() ? 'is-selected' : ''}>
+                    <input
+                      type="checkbox"
+                      checked={selected()}
+                      onchange={() => handleCategoryToggle(item)}
+                    />
+                    <span>{item}</span><br />
+                  </label>
+                )
+              }} />
+            </div>
+          </div>
         </div>
       </div>
+      
       <div class="product-catalog__list">
-        <span>{data.loading && "Loading..."}</span>
-        <For each={data()?.products} children={(item: any, i) => (
-          <a href={`/product/${item.id}`} class="product-catalog__item">
-            <img src={item.thumbnail} class="product-catalog__img" />
-            <div class="product-catalog__info">
-              
-            </div>
-          </a>
-        )} />
+        <div class="product-catalog__wrapper">
+          <Show
+            when={!data.loading}
+            fallback={() => (
+              <For each={Array(6).fill(1)} children={() => (
+                <div class="product-catalog__item">
+                  <div class="product-catalog__img">
+                    <Loader />
+                  </div>
+                </div>
+              )} />
+            )}
+            children={() => (
+              <For each={data()?.products} children={(item: any, i) => (
+                <a href={`/product/${item.id}`} class="product-catalog__item">
+                  <img src={item.thumbnail} class="product-catalog__img" />
+                  <div class="product-catalog__info">
+                    
+                  </div>
+                </a>
+              )} />
+            )}
+          />
+        </div>
       </div>
     </div>
   );
